f58128ee30ee33447462b9e19006e35d7f927fd5
lekangwang <lekang.wang@yahoo.ca>
2021-03-20 15:13:23 -0400

    added styles for all modals and implemented chart js boilerplate

---
+++ js/chart.js
@@ -0,0 +1,33 @@
+"use strict";
+const pomatoChart = document.getElementById("pomato-chart").getContext("2d");
+const myChart = new Chart(pomatoChart, {
+  type: "bar",
+  data: {
+    labels: ["Work", "Break"],
+    datasets: [
+      {
+        label: "# of minutes",
+        data: [0, 0],
+        backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)"],
+        borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)"],
+        borderWidth: 1,
+        responsive: true,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+            precision: 0,
+            stepSize: 1,
+          },
+        },
+      ],
+    },
+    responsive: true,
+    maintainAspectRatio: false,
+  },
+});
